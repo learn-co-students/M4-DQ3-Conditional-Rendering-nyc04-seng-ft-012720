9493a4f170c2c8f87c743f18aba710c9750ff3fe
abidtalukdar <abiditalukdar@gmail.com>
2020-04-02 11:57:35 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-const MenuBar = (props) => {
-
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
@@ -13,22 +11,32 @@ const MenuBar = (props) => {
 
   */
 
+ const MenuBar = ({changeSelected, selected}) => {
+
+    const handleClick = (event) => {
+      changeSelected(event.target.id)
+    }
+
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className={"item " + (selected==="profile" ? "active" : null)} 
+         id="profile" onClick={handleClick}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className={"item " + (selected==="photo" ? "active" : null)}
+        id="photo" onClick={handleClick}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className={"item " + (selected==="cocktail" ? "active" : null)}
+        id="cocktail" onClick={handleClick}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a className={"item " + (selected==="pokemon" ? "active" : null)}
+        id="pokemon" onClick={handleClick}>
+        <i className="themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
